5515d46330352d66f4f4b7280e9f566097457b35
brandon park <brandonpark206@gmail.com>
2023-11-30 00:43:26 -0800

    add create DAO and unit tests

---
+++ src/test/java/com/bpark/database/DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpark.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
