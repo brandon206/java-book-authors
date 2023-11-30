dca1447ef4a7b7fb9c029b89ca9d6e8b51bcfbb6
brandon park <brandonpark206@gmail.com>
2023-11-30 00:40:07 -0800

    add create DAOs for book and author

--- src/main/java/com/bpark/database/dao/AuthorDao.java
+++
@@ -1,7 +0,0 @@
-package com.bpark.database.dao;
-
-import com.bpark.database.domain.Author;
-
-public interface AuthorDao {
-    void create(Author author);
-}
--- src/main/java/com/bpark/database/dao/BookDao.java
+++
@@ -1,8 +0,0 @@
-package com.bpark.database.dao;
-
-import com.bpark.database.domain.Book;
-
-public interface BookDao {
-    void create(Book book);
-
-}
